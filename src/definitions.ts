08eb74df1d4167f3d9e211f6cf4b5b70f19f1dba
himanshu-rathore-5exe <himanshu.rathore@5exceptions.com>
2025-06-02 18:53:07 +0530

    fix

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,4 +1,5 @@
 export interface TruvideoSdkImagePlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
-  editImage(options: { value: string }): Promise<{ value: string }>;
+    echo(options: { value: string }): Promise<{ value: string }>;
+
+    editImage(options: { inputPath: string; outputPath: string }): Promise<{ result: string }>;
 }
