c29a3377d6d752798b5755c60c1b38ad32c4a1aa
Akash Gupta <khush@Akash-MacBook-Air.local>
2026-04-22 12:45:52 +0530

    Images fixed now working

--- ios/Sources/TruvideoSdkImagePlugin/TruvideoSdkImagePlugin.swift
+++ ios/Sources/TruvideoSdkImagePlugin/TruvideoSdkImagePlugin.swift
@@ -1,67 +1,67 @@
-// import Foundation
-// import Capacitor
-// import TruvideoSdkImage
+import Foundation
+import Capacitor
+import TruvideoSdkImage
 
-// /**
-//  * Please read the Capacitor iOS Plugin Development Guide
-//  * here: https://capacitorjs.com/docs/plugins/ios
-//  */
-// @objc(TruvideoSdkImagePlugin)
-// public class TruvideoSdkImagePlugin: CAPPlugin, CAPBridgedPlugin {
-//     public let identifier = "TruvideoSdkImagePlugin"
-//     public let jsName = "TruvideoSdkImage"
-//     public let pluginMethods: [CAPPluginMethod] = [
-//         CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise),
-//         CAPPluginMethod(name: "editImage", returnType: CAPPluginReturnPromise)
-//     ]
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitorjs.com/docs/plugins/ios
+ */
+@objc(TruvideoSdkImagePlugin)
+public class TruvideoSdkImagePlugin: CAPPlugin, CAPBridgedPlugin {
+    public let identifier = "TruvideoSdkImagePlugin"
+    public let jsName = "TruvideoSdkImage"
+    public let pluginMethods: [CAPPluginMethod] = [
+        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "editImage", returnType: CAPPluginReturnPromise)
+    ]
 
 
-//     @objc func echo(_ call: CAPPluginCall) {
-//         let value = call.getString("value") ?? ""
-//         call.resolve([
-//             "value": value
-//         ])
-//     }
-//     @objc func editImage(_ call: CAPPluginCall) {
-//         let inputPath = call.getString("inputPath") ?? ""
-//         let outputPath = call.getString("outputPath") ?? ""
-//         launchImageEdit(inputPath: inputPath, outputPath: fileURLString(from: outputPath), call)
-//     }
-//     func fileURLString(from outputPath: String) -> String {
-//         if outputPath.hasPrefix("file://") {
-//             return outputPath
-//         } else {
-//             return "file://\(outputPath)"
-//         }
-//     }
-//     func launchImageEdit(inputPath: String,outputPath: String,_ call: CAPPluginCall) {
-//            Task{
-//                guard let rootViewController = await UIApplication.shared.keyWindow?.rootViewController else {
-//                    print("E_NO_ROOT_VIEW_CONTROLLER", "No root view controller found")
-//                    let error = NSError(domain: "com.TruvideoImageSDk.ImageSDK", code: 500, userInfo: [NSLocalizedDescriptionKey: "No root view controller found"])
-//                    call.reject("E_NO_ROOT_VIEW_CONTROLLER", "No root view controller foundh",error)
-//                    return
-//                }
-//                if let imageURL = URL(string: inputPath) ,let outputURL = URL(string:fileURLString(from: outputPath)) {
-//                         let output: TruvideoSdkImageFileDescriptor = .custom(rawPath: outputURL.absoluteString)
-//                         let configuration = TruvideoSdkImageEditorPreset(imageURL: imageURL, output: output)
+    @objc func echo(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": value
+        ])
+    }
+    @objc func editImage(_ call: CAPPluginCall) {
+        let inputPath = call.getString("inputPath") ?? ""
+        let outputPath = call.getString("outputPath") ?? ""
+        launchImageEdit(inputPath: inputPath, outputPath: fileURLString(from: outputPath), call)
+    }
+    func fileURLString(from outputPath: String) -> String {
+        if outputPath.hasPrefix("file://") {
+            return outputPath
+        } else {
+            return "file://\(outputPath)"
+        }
+    }
+    func launchImageEdit(inputPath: String,outputPath: String,_ call: CAPPluginCall) {
+           Task{
+               guard let rootViewController = await UIApplication.shared.keyWindow?.rootViewController else {
+                   print("E_NO_ROOT_VIEW_CONTROLLER", "No root view controller found")
+                   let error = NSError(domain: "com.TruvideoImageSDk.ImageSDK", code: 500, userInfo: [NSLocalizedDescriptionKey: "No root view controller found"])
+                   call.reject("E_NO_ROOT_VIEW_CONTROLLER", "No root view controller foundh",error)
+                   return
+               }
+               if let imageURL = URL(string: inputPath) ,let outputURL = URL(string:fileURLString(from: outputPath)) {
+                        let output: TruvideoSdkImageFileDescriptor = .custom(rawPath: outputURL.absoluteString)
+                        let configuration = TruvideoSdkImageEditorPreset(imageURL: imageURL, output: output)
          
                    
-//                    await rootViewController.presentTruvideoSdkImageEditorView(preset: configuration, onComplete: { result in
-//                        //call.resolve(["result": result.editedImageUrl.absoluteString])
-//                        if let editedImageUrl: URL = result.editedImageURL {
-//                            do{
-//                                 call.resolve(["result": editedImageUrl.absoluteString])
-//                            }
-//                        } else{
-//                            call.resolve(["result": ""])
-//                        }
-//                    })
-//                } else{
-//                    let error = NSError(domain:"com.TruvideoImageSDk.ImageSDK", code: 500, userInfo: [NSLocalizedDescriptionKey: "Failed to get directory path"])
-//                    call.reject("NO_PATH_Found", "Failed to get directory path", error)
-//                }
-//            }
-//        }
+                   await rootViewController.presentTruvideoSdkImageEditorView(preset: configuration, onComplete: { result in
+                       //call.resolve(["result": result.editedImageUrl.absoluteString])
+                       if let editedImageUrl: URL = result.editedImageURL {
+                           do{
+                                call.resolve(["result": editedImageUrl.absoluteString])
+                           }
+                       } else{
+                           call.resolve(["result": ""])
+                       }
+                   })
+               } else{
+                   let error = NSError(domain:"com.TruvideoImageSDk.ImageSDK", code: 500, userInfo: [NSLocalizedDescriptionKey: "Failed to get directory path"])
+                   call.reject("NO_PATH_Found", "Failed to get directory path", error)
+               }
+           }
+       }
     
-// }
+}
